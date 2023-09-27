14c0cdb346a20d05f5f1f67b2e1a59450501d332
nikivikk <nikiforenko2110@gmail.com>
2023-09-28 00:41:07 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+import moviepy.editor
+from pathlib import Path
+video_file = Path("coldplay.mp4")
+video = moviepy.editor.VideoFileClip(f'{video_file}')
+audio = video.audio
+audio.write_audiofile(f'{video_file.stem}.mp3')
